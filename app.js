04b918941661dbd69c1042f2b7b4763d05a291eb
Yuki Iwami <yiwami00@mylangara.ca>
2026-02-05 02:20:19 +0000

    Task6: add EJS templates for success and error

--- app.js
+++ app.js
@@ -1,10 +1,11 @@
 import express from "express";
-import { validatePizza, validateColor } from "./my-validators.js";
+import { validatePizza } from "./my-validators.js";
 
 const app = express();
 
-app.use(express.static("public"));
+app.set("view engine", "ejs");
 
+app.use(express.static("public"));
 app.use(express.urlencoded({ extended: true }));
 
 app.listen(3000, () => {
@@ -13,8 +14,8 @@ app.listen(3000, () => {
 
 app.post("/orders", validatePizza, (req, res) => {
   if (res.locals.errors.length === 0) {
-    res.json(req.body);
+    res.render("success", { name: req.body.name, email: req.body.email });
   } else {
-    res.status(422).json(res.locals.errors);
+    res.status(422).render("error", { errors: res.locals.errors });
   }
 });
