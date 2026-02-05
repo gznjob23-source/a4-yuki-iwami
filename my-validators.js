4dd537b57cf9b3113b7d1e48eb81b76a2b7ee860
Yuki Iwami <yiwami00@mylangara.ca>
2026-02-05 05:55:57 +0000

    Task7 done

--- my-validators.js
+++ my-validators.js
@@ -8,17 +8,26 @@ export const validatePizza = (req, res, next) => {
     res.locals.errors.push({ field: "sauce", message: "Invalid sauce." });
   }
 
-  const validToppings = ["pepperoni", "mushrooms", "olives"];
+  const validToppings = ["pepperoni", "ham", "vegetariansausage", "mushrooms", "peppers", "olives"];
   let toppings = req.body.toppings;
 
-  if (toppings === undefined) toppings = [];
-  if (!Array.isArray(toppings)) toppings = [toppings];
+  if (toppings === undefined) {
+    toppings = [];
+  } else if (typeof toppings === "string") {
+    toppings = [toppings];
+  }
 
   if (toppings.length < 1 || toppings.length > 3) {
     res.locals.errors.push({ field: "toppings", message: "Choose 1 to 3 toppings." });
   }
 
-  if (!toppings.every((t) => validToppings.includes(t))) {
+  let invalidFound = false;
+  for (let i = 0; i < toppings.length; i++) {
+    if (!validToppings.includes(toppings[i])) {
+      invalidFound = true;
+    }
+  }
+  if (invalidFound) {
     res.locals.errors.push({ field: "toppings", message: "Invalid topping." });
   }
 
