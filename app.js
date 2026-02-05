68242fdae2edde47525d4c946a76f25e7573cd95
Yuki Iwami <yiwami00@mylangara.ca>
2026-02-05 00:58:19 +0000

    Task 2: add static and urlencoded middleware

--- app.js
+++ app.js
@@ -2,6 +2,11 @@ import express from "express";
 
 const app = express();
 
+app.use(express.static("public"));
+
+app.use(express.urlencoded({ extended: true }));
+
 app.listen(3000, () => {
   console.log("Server ready!");
 });
+
